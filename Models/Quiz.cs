34f41fced268f328e49be5ff2c8514e94c02b94c
bielak84 <pzx107152@student.poznan.merito.pl>
2024-02-20 19:50:53 +0100

    add class quiz

---
+++ Models/Quiz.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Quiz
+{
+	public Quiz()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
