502deca635b793f88f01eca43960805ca243874c
bielak84 <pzx107152@student.poznan.merito.pl>
2024-02-22 12:12:39 +0100

    sql base creator

---
+++ Models/Pytanie.cs
@@ -0,0 +1,17 @@
+﻿//prop
+//ctor
+
+namespace Trivio.Models
+{
+    public class Pytanie
+    {
+        public int Id { get; set; }
+        public int IdQuizz { get; set; }
+        public string TrescPytania { get; set; }
+
+        public Pytanie()
+        {
+               
+        }
+    }
+}
---
+++ Models/Quizz.cs
@@ -0,0 +1,16 @@
+﻿//prop
+//ctor
+namespace Trivio.Models
+{
+    public class Quizz
+    {
+        public int Id { get; set; }
+        public string IdAspNetUsers { get; set; }
+        public int Iloscpytan { get; set; }
+        public string CreateDateTime { get; set; }
+        public Quizz()
+        {
+         
+        }
+    }
+}
