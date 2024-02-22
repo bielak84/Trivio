9edef06fb4274d29a1571f39668e551b3fc80396
bielak84 <pzx107152@student.poznan.merito.pl>
2024-02-22 14:02:34 +0100

    update model to 1 file

--- Models/Quizz.cs
+++ Models/Quizz.cs
@@ -13,4 +13,28 @@ namespace Trivio.Models
          
         }
     }
+    public class Pytanie
+    {
+        public int Id { get; set; }
+        public int IdQuizz { get; set; }
+        public string TrescPytanie { get; set; }
+
+        public Pytanie()
+        {
+
+        }
+    }
+    public class Odpowiedz
+    {
+        public int Id { get; set; }
+        public int IdPytanie { get; set; }
+        public string TrescOdpowiedz { get; set; }
+        public bool poprawna { get; set; }
+
+        public Odpowiedz()
+        {
+
+        }
+    }
+
 }
